13d078d31f7b0797f54cd8ed8a7e8b2f32043686
yehanportilla <jancha22@gmail.com>
2019-10-10 10:52:55 -0500

    Se agrega atributos o tags perzonalizados para zipkin

--- src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
 import com.formacionbdi.springboot.app.usuarios.commons.models.entity.Usuario;
 
+import brave.Tracer;
 import feign.FeignException;
 
 @Service
@@ -27,7 +28,9 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
 	@Autowired
 	private UsuarioFeignClient client;
 
-	
+	@Autowired
+	private Tracer tracer; // para añadir mas informacion a la traza
+
 	/**
 	 * Metodo encargado de obtener el usuario por el username usando el
 	 * clienteHttp(UsuarioFeignClient)
@@ -35,20 +38,24 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		try {
-			  Usuario usuario = client.findByUsername(username);
+			Usuario usuario = client.findByUsername(username);
 
-			 List<GrantedAuthority> authorities = usuario.getRoles().stream()
+			List<GrantedAuthority> authorities = usuario.getRoles().stream()
 					.map(role -> new SimpleGrantedAuthority(role.getNombre()))
 					.peek(authority -> log.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
 
-			 log.info("Usuario autenticado: " + username);
+			log.info("Usuario autenticado: " + username);
 
-			 return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
+			return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
 					authorities);
 
 		} catch (FeignException e) {
-			log.error("Error en el login, no existe el usaurio '" + username + "' en el sistema");
-			throw new UsernameNotFoundException("Error en el login, no existe el usaurio '" + username + "' en el sistema");
+
+			String error = "Error en el login, no existe el usaurio '" + username + "' en el sistema";
+			log.error(error);
+			// muestra el mensaje en caso de error en zipkin, se agrega al span
+			tracer.currentSpan().tag("error.mensaje", error + ": " + e.getMessage());
+			throw new UsernameNotFoundException(error);
 		}
 	}
 
